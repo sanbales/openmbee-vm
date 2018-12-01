69d8ac7f3c014c38b34ee943a4db05b13b6b52a2
Santiago Balestrini <santiago.balestrini@gtri.gatech.edu>
2018-11-30 21:29:54 -0500

    Updated provision script to fix indentations and the vm.max_map_count setting

--- provision.sh
+++ provision.sh
@@ -2,18 +2,33 @@
 DOCKER_COMPOSE_BASE_URL="https://github.com/docker/compose/releases/download"
 DOCKER_COMPOSE_LOCATION=/usr/local/bin/docker-compose
 DOCKER_COMPOSE_VERSION="1.23.2"
+ES_MAX_MAP_COUNT=262144
+ES_SYSCTL_CONF_FILE=/usr/lib/sysctl.d/90-elastic-search.conf
 
 
-if ! (command -v docker); then
-    echo ">>> Installing Docker"
-    yum -y install docker
+# This is required by Elastic Search, otherwise it will crash, as described here:
+# https://www.elastic.co/guide/en/elasticsearch/reference/current/vm-max-map-count.html
+if [[ "$(sysctl -n vm.max_map_count)" -ne "$ES_MAX_MAP_COUNT" ]]; then
+  echo ">>> Setting the maximum number of memory map areas a process may have to $(ES_MAX_MAP_COUNT)"
+  sudo sysctl -w vm.max_map_count=${ES_MAX_MAP_COUNT}
+
+  # Make vm.max_map_count setting permanent
+  if [[ ! -f ${ES_SYSCTL_CONF_FILE} ]]; then
+    echo "vm.max_map_count=$ES_MAX_MAP_COUNT" | sudo tee -a ${ES_SYSCTL_CONF_FILE}
+  fi
 fi
 
 
-if ! (grep -q -E "^docker:" /etc/group); then
-    echo ">>> Creating a docker user group and adding vagrant user to it"
-    groupadd docker
-    usermod -aG docker vagrant
+if ! ( command -v docker ); then
+  echo ">>> Installing Docker"
+  yum -y install docker
+fi
+
+
+if ! ( grep -q -E "^docker:" /etc/group ); then
+  echo ">>> Creating a docker user group and adding vagrant user to it"
+  groupadd docker
+  usermod -aG docker vagrant
 fi
 
 
@@ -34,13 +49,6 @@ if [[ ! -f ${DOCKER_COMPOSE_LOCATION} ]]; then
     sudo curl -L "$DOCKER_COMPOSE_BASE_URL/$DOCKER_COMPOSE_VERSION/docker-compose-$(uname -s)-$(uname -m)" \
               -o "$DOCKER_COMPOSE_LOCATION"
     sudo chmod +x "$DOCKER_COMPOSE_LOCATION"
-fi
-
-
-# This is required by Elastic Search, otherwise it will crash
-if ! ( sysctl vm.max_map_count | grep -q -E "= 262144" ); then
-  echo ">>> Setting the maximum number of memory map areas a process may have to 262,144"
-  sudo sysctl -w vm.max_map_count=262144
 fi
 
 
